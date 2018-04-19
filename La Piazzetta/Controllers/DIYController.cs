4b5f8f665f32ebd6f895b02e1a7c9f12a4b4f6d5
Diana Lazar <lazardiana28@yahoo.com>
2018-04-19 15:57:12 +0300

    add diy

---
+++ La Piazzetta/Controllers/DIYController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using La_Piazzetta.Handlers;
+using La_Piazzetta.Models;
+
+namespace La_Piazzetta.Controllers
+{
+    public class DIYController : Controller
+    {
+        public ActionResult DIY()
+        {
+            var model = new List<Ingredient>();
+            model = IngredientHandler.Instance.GetAll().Ingredients.ToList();
+            return View(model);
+        }
+    }
+}
